1628a5fc4cdb79114de317aba01e071112df280b
ankitsinghss673-hash <ankitsingh9311143@gmail.com>
2026-09-15 17:46:45 +0530

    Pattern Question 2

--- Pattern2.cpp
+++ Pattern2.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter the number here:"<<endl;
+    cout<<"Enter the number Here:"<<endl;
     cin>>n;
-    
+    char ch='a';
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j+=1){
-            cout<<"*"<<" ";
- 
+            cout<<ch<<" ";
+            
         }
+        ch+=1;
         cout<<endl;
     }
 }
