672ea12c92b98fa7c3dec03faa0a198ff41b7331
ankitsinghss673-hash <ankitsingh9311143@gmail.com>
2026-09-16 14:53:46 +0530

    More Patterns

--- Pattern5.cpp
+++ Pattern5.cpp
@@ -12,6 +12,8 @@ int main(){
         }
         cout<<endl;
     }
+    cout<<endl;
+    cout<<"Another Answer"<<endl;
     char ch= 'a' ;
     for(int i=1; i<=n; i++ ){
         for(int j=1; j<=i; j++){
@@ -21,5 +23,39 @@ int main(){
         ch+=1;
         cout<<endl;
     }
+    cout<<endl;
+    cout<<"Another Answer "<<endl;
+
+    
+    for(int i=n; i>=1; i--){
+        for(int j=i; j>=1; j--){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Anothet Answer"<<endl;
+    int count=1;
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            cout<<j<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<"Another Answer"<<endl;
+
+    for(int i=1; i<=n; i++){
+        for(int j=n; j>=n-i+1; j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+        
+    
+    
 
 }
