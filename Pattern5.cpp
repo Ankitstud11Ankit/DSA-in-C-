94df8e3f85c4be7f0a6a05392c761ccab014030a
ankitsinghss673-hash <ankitsingh9311143@gmail.com>
2026-09-16 13:36:49 +0530

    New Addition

--- Pattern5.cpp
+++ Pattern5.cpp
@@ -12,4 +12,14 @@ int main(){
         }
         cout<<endl;
     }
+    char ch= 'a' ;
+    for(int i=1; i<=n; i++ ){
+        for(int j=1; j<=i; j++){
+            cout<<ch<<" ";
+
+        }
+        ch+=1;
+        cout<<endl;
+    }
+
 }
